50540c7255e3f7fc9d23251b980d0a177d732286
Lizzie <liz.gardiner@btinternet.com>
2026-02-13 15:29:34 +0000

    add test for clicking

--- tests/test_album_routes.py
+++ tests/test_album_routes.py
@@ -14,6 +14,24 @@ def test_get_albums(db_connection, web_client, page, test_web_address):
     expect(a).to_have_text(['\nTitle: Title 1\nReleased: 1998\n', '\nTitle: Title 2\nReleased: 2007\n', '\nTitle: Another title\nReleased: 2023\n', '\n            Title: Final title\nReleased: 1956\n'])
     expect(p).to_have_text(['Title: Title 1 Released: 1998', 'Title: Title 2 Released: 2007', 'Title: Another title Released: 2023', 'Title: Final title Released: 1956'])
 
+def test_clicking_album_shows_page(db_connection, page, test_web_address):
+    db_connection.seed('seeds/albums_table.sql')
+    page.goto(f"http://{test_web_address}/albums")
+    page.get_by_text('Title 1').click()
+    h1 = page.locator("h1")
+    p = page.locator("p")
+    expect(h1).to_have_text("Title 1")
+    expect(p).to_have_text("Release year: 1998 Artist: Taylor Swift")
+    page.goto(f"http://{test_web_address}/albums")
+    h1 = page.locator("h1")
+    expect(h1).to_have_text("Albums")
+    page.get_by_text('Another title').click()
+    h1 = page.locator("h1")
+    expect(h1).to_have_text("Another title")
+    p = page.locator("p")
+    expect(p).to_have_text("Release year: 2023 Artist: Pixies")
+
+
     # Notice that if I'm asserting on the a tag, I need the \n. But if I assert on the p, there are no \n.
 
     # below is an alernative way. to_have_text needs an exact match, whereas to_contain_text just needs a partial string (i.e. contained within it). We can also access a specific tag with .nth(0) rather than asserting them all in a list.
@@ -41,7 +59,7 @@ def test_get_one_album(db_connection, web_client, page, test_web_address):
     expect(h1).to_have_text("Title 1")
     expect(p).to_have_text("Release year: 1998\nArtist: Taylor Swift")
     
-'''
+
 def test_post_album(db_connection, web_client, page, test_web_address):
     db_connection.seed('seeds/albums_table.sql')
     response = web_client.post('/albums', data = {'title': 'Voyage', 'release_year': 2022, 'artist_id': 2})
@@ -56,7 +74,7 @@ def test_post_album(db_connection, web_client, page, test_web_address):
     expect(p).to_contain_text("Title: Voyage")
     expect(p).to_contain_text("Released: 2022")
     #assert response_2.data.decode('utf-8') == "Album(1, Title 1, 1998, 3), Album(2, Title 2, 2007, 4), Album(3, Another title, 2023, 1), Album(4, Final title, 1956, 3), Album(5, Voyage, 2022, 2)"
-    '''
+    
 
 def test_post_album_no_params(db_connection, web_client):
     db_connection.seed('seeds/albums_table.sql')
